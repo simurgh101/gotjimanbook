860d82c0c56b6fa633f011ab9a4e11363e0b8abf
Simurgh101(Scotth) <suthiwatpraesi@hotmail.com>
2020-11-24 17:56:14 +1100

    nav date

--- src/Navbars.js
+++ src/Navbars.js
@@ -3,7 +3,7 @@ import React from "react";
 const Navbars = () => {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="#">
           Navbar
         </a>
